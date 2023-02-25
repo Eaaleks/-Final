2387dc48661256485c78878082a29bde0ee1ca18
thenewlife <thenewlife@mail.ru>
2023-02-25 13:04:20 +0300

    добавил финальный урок

---
+++ Task64/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+void PrintNumbers(int number)
+{
+    if (number < 1) // базовый случай, когда n становится меньше 1
+    {
+        return;
+    }
+    Console.Write(number + ", ");
+    PrintNumbers(number - 1); // вызываем функцию с аргументом на единицу меньше текущего значения
+}
+
+int number = GetNumber("Введите число больше 0:");
+PrintNumbers(number);
---
+++ Task66/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int SumNaturalNumbers(int m, int n)
+{
+    int sum = 0;
+
+    for (int i = m; i <= n; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int m = GetNumber("Введите число");
+int n = GetNumber("Введите число");
+int sum = SumNaturalNumbers(m, n);
+
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {m} до {n} равна {sum}");
+
